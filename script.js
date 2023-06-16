c14d03b707a9ddd47caf5ea278acd8ac7eb4fb4c
Jay Termulo <jetermulo@gmail.com>
2023-06-16 10:21:21 +0800

    Initial Mark Up

---
+++ script.js
@@ -0,0 +1,11 @@
+// VARIABLE
+const name = document.querySelector(`CSS-Selector`);
+// EVENT LISTENER
+Node.addEventListener('event-name', callback-func);
+// FUNCTION
+function myFunction(a, b) {
+    return a + b ;
+}
+myFunction(1, 2); /* answer: 3*/
+
+VBArray.addEventListener('click', push);
